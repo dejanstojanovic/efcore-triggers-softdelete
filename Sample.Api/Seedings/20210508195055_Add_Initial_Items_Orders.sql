7bec8394ae348ae1c76865d3b81a5796725210f7
Dejan Stojanovic <d.stojanovic@hotmail.com>
2021-05-08 20:47:34 +0400

    feat: adds initial seeding

--- Sample.Api/Seedings/20210508195055_Add_Initial_Items_Orders.sql
+++ Sample.Api/Seedings/20210508195055_Add_Initial_Items_Orders.sql
@@ -1,3 +1,19 @@
-/******** Add you seeeding script here ********/
+DECLARE @mouseId INT
+INSERT INTO Items([Name], [Description]) Values ('Logitech MX Master 2s', 'Logitech wireless mouse')
+SET @mouseId = @@IDENTITY
 
---INSERT INTO Items (
+DECLARE @keyboardId INT
+INSERT INTO Items([Name], [Description]) Values ('Microsoft Sculpt Ergonomic keyboard', 'Microsoft ergonomic keyboard')
+SET @keyboardId = @@IDENTITY
+
+DECLARE @speakersId INT
+INSERT INTO Items([Name], [Description]) Values ('CREATIVE Pebbles', 'Desktop speakers')
+SET @speakersId = @@IDENTITY
+
+DECLARE @orderId INT
+INSERT INTO Orders(CreatedOn) values (GETUTCDATE())
+SET @orderId = @@IDENTITY
+
+INSERT INTO OrderItems(OrderId, ItemId, Quantity) Values (@orderId, @mouseId, 3)
+INSERT INTO OrderItems(OrderId, ItemId, Quantity) Values (@orderId, @keyboardId, 1)
+INSERT INTO OrderItems(OrderId, ItemId, Quantity) Values (@orderId, @speakersId, 2)
